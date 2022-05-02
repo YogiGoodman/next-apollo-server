5f3a42bdcbd07ed9e83493ef00169bba22272a37
Yogeshkumar Borkar <borkar.yogeshkumar@gmail.com>
2022-05-02 17:38:33 +0100

    code cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,20 @@
 import { ApolloServer } from "apollo-server-express";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import express from "express";
 import http from "http";
-import schemaWithMocks from "./schema";
-import { PersonSchema } from "./schema";
-import { PersonDataMock } from "./mock";
+import schemaWithMocks from "./mock";
 
 async function listen(port: number) {
   const app = express();
   const httpServer = http.createServer(app);
   const corsOptions = {
-    origin: ["http://localhost:3000", "https://studio.apollographql.com"],
+    origin: [
+      "http://localhost:3000",
+      "https://nextjs-apollo-5szlld6zy-yogigoodman.vercel.app",
+      "https://studio.apollographql.com",
+    ],
   };
   const server = new ApolloServer({
     schema: schemaWithMocks,
-    // typeDefs: PersonSchema,
-    // mocks: PersonDataMock,
-    // plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
   await server.start();
 
@@ -33,7 +31,7 @@ async function listen(port: number) {
 async function main() {
   try {
     await listen(5000);
-    console.log("Server is ready at http://localhost:5000/graphql");
+    console.log("Server is ready");
   } catch (err) {
     console.error("Error starting the node server", err);
   }
